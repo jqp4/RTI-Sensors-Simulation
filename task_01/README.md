85d7a29223e1791ec16d3b9630a70d938ea64d7a
Gleb <skryabin.gleb.d@gmail.com>
2022-09-29 05:16:34 +0300

    worked point trajectory

--- task_01/README.md
+++ task_01/README.md
@@ -1,23 +1,23 @@
 # Задача 1
+
 дедлайн: пн, 26.09.2022
 
+## Заметки к коду
 
-# Заметки к коду
-* vp examples https://www.glowscript.org/#/user/GlowScriptDemos/folder/Examples/
-* vp.label https://www.glowscript.org/docs/VPythonDocs/label.html
-* build grid https://youtu.be/c-abpp3xS2Y
-* vp.scene control https://www.youtube.com/watch?v=9X1taTrS7Ng
-* sy.integrate https://www.geeksforgeeks.org/how-to-find-definite-integral-using-python/
+* vp examples <https://www.glowscript.org/#/user/GlowScriptDemos/folder/Examples/>
+* vp.label <https://www.glowscript.org/docs/VPythonDocs/label.html>
+* build grid <https://youtu.be/c-abpp3xS2Y>
+* vp.scene control <https://www.youtube.com/watch?v=9X1taTrS7Ng>
+* sy.integrate <https://www.geeksforgeeks.org/how-to-find-definite-integral-using-python/>
 
+## Заметки к отчету
 
-# Заметки к отчету
-* Параметрическое задание кривой https://lfirmal.com/parametricheskoe-zadanie-krivoj/
-* Длина дуги кривой, заданной в параметрическом виде https://portal.tpu.ru/SHARED/k/KONVAL/Sites/Russian_sites/Calc1-ru/12/22a.htm
-* 
+* Параметрическое задание кривой <https://lfirmal.com/parametricheskoe-zadanie-krivoj/>
+* Длина дуги кривой, заданной в параметрическом виде <https://portal.tpu.ru/SHARED/k/KONVAL/Sites/Russian_sites/Calc1-ru/12/22a.htm>
+* Параметрические уравнения четырехугольников <http://stob2.narod.ru/zetirexygol/zetirexygol.htm>
 
+## Плейлисты лекций
 
-# Плейлисты лекций
-* vpython https://www.youtube.com/watch?v=vEMCiugDnKI&list=PLdCdV2GBGyXOnMaPS1BgO7IOU_00ApuMo
-* vpython https://www.youtube.com/watch?v=MJiVtz4Uj7M&list=PLGs0VKk2DiYzGCOzBrMNSWEdd2CIGC0kJ
+* vpython <https://www.youtube.com/watch?v=vEMCiugDnKI&list=PLdCdV2GBGyXOnMaPS1BgO7IOU_00ApuMo>
+* vpython <https://www.youtube.com/watch?v=MJiVtz4Uj7M&list=PLGs0VKk2DiYzGCOzBrMNSWEdd2CIGC0kJ>
   
-
--- task_01/task_01.py
+++ task_01/task_01.py
@@ -179,17 +179,15 @@ class Sensor:
         self.label.delete()
 
 
-class Circle_Trajectory:
-    def __init__(self, R, x0, y0, z0):
-        self.R = R
+class Trajectory:
+    def __init__(self, x0=0.0, y0=0.0, z0=0.0):
         self.x0 = x0
         self.y0 = y0
         self.z0 = z0
         self.deg_to_rad = lambda deg: deg * sy.pi / 180.0
-        self.get_curve_length()
-        print(f"Trajectory curve length = {self.curve_length}")
+        self.curve_length = self.get_curve_length()
 
-    def get_curve_length(self):
+    def get_curve_length(self) -> float:
         f1 = lambda t: sy.sqrt(
             self.derivative_f_x(t) ** 2
             + self.derivative_f_y(t) ** 2
@@ -197,10 +195,35 @@ class Circle_Trajectory:
         )
 
         t_symb = sy.Symbol("t")
-        self.curve_length = sy.N(sy.integrate(f1(t_symb), (t_symb, 0, math.pi * 2)))
+        return sy.N(sy.integrate(f1(t_symb), (t_symb, 0, math.pi * 2)))
 
     def get_xyz(self, t) -> tuple:
         return (self.x0 + self.f_x(t), self.y0 + self.f_y(t), self.z0 + self.f_z(t))
+
+    def f_x(self, t) -> float:
+        return 0.0
+
+    def f_y(self, t) -> float:
+        return 0.0
+
+    def f_z(self, t) -> float:
+        return 0.0
+
+    def derivative_f_x(self, t) -> float:
+        return 0.0
+
+    def derivative_f_y(self, t) -> float:
+        return 0.0
+
+    def derivative_f_z(self, t) -> float:
+        return 0.0
+
+
+class Circle_XY_Trajectory(Trajectory):
+    def __init__(self, R, x0=0.0, y0=0.0, z0=0.0):
+        self.R = R
+        super().__init__(x0, y0, z0)
+        print(f"Circle_XY_Trajectory(R={R}).curve_length = {self.curve_length}")
 
     def f_x(self, t) -> float:
         return self.R * sy.cos(self.deg_to_rad(t))
@@ -208,38 +231,140 @@ class Circle_Trajectory:
     def f_y(self, t) -> float:
         return self.R * sy.sin(self.deg_to_rad(t))
 
-    def f_z(self, t) -> float:
-        return 0
-
     def derivative_f_x(self, t) -> float:
         return -self.R * sy.sin(self.deg_to_rad(t))
 
     def derivative_f_y(self, t) -> float:
         return self.R * sy.cos(self.deg_to_rad(t))
 
-    def derivative_f_z(self, t) -> float:
-        return 0
+
+# криво работает
+class Rectangle_XY_Trajectory(Trajectory):
+    def __init__(self, P, L, x0=0.0, y0=0.0, z0=0.0):
+        """* P - Длина диагонали прямоугольник;
+        * L - Угол наклона диагонали к горизонтали в радианах
+        * x0, y0, z0 - смещение траектории по координатной сетке"""
+
+        self.P = P
+        self.L = L
+        # # itp = lambda T: int(T / P)  # int [T / P]
+        # self.A1 = lambda T: int(1 / int(T / P)) * int(T / P)
+        # self.A2 = lambda T: int(2 / int(T / P)) * int(int(T / P) / 2)
+        # self.A3 = lambda T: int(3 / int(T / P)) * int(int(T / P) / 3)
+        # self.A4 = lambda T: int(4 / int(T / P)) * int(int(T / P) / 4)
+        # self.A5 = lambda T: T - P * int(T / P)
+        super().__init__(x0, y0, z0)
+        print(
+            f"Rectangle_XY_Trajectory(P={P}, L={L}).curve_length = {self.curve_length}"
+        )
+
+    # T in [P, 5P]
+    def calculate_A_values(self, t):
+        """* t - degree"""
+        # t degree [0, 360) -> float T in [P, 5P]
+        T = (((t % 360) / 360) * 4 + 1) * self.P
+
+        k = T / self.P
+        vp.scene.caption = f"T in [P, 5P] = {k}\n"
+
+        itp = int(T / self.P)
+        self.A1 = int(1 / itp) * itp
+        self.A2 = int(2 / itp) * int(itp / 2)
+        self.A3 = int(3 / itp) * int(itp / 3)
+        self.A4 = int(4 / itp) * int(itp / 4)
+        self.A5 = T - self.P * itp
+
+    def f_x(self, t) -> float:
+        self.calculate_A_values(t)
+        # return ((self.A2(t) + self.A3(t)) * self.A5(t) + self.A4(t) * self.P) * sy.cos(self.L)
+        return ((self.A2 + self.A3) * self.A5 + self.A4 * self.P) * sy.cos(self.L)
+
+    def f_y(self, t) -> float:
+        self.calculate_A_values(t)
+        # return ((self.A1(t) + self.A4(t)) * self.A5(t) + self.A3(t) * self.P) * sy.sin(self.L)
+        return ((self.A1 + self.A4) * self.A5 + self.A3 * self.P) * sy.sin(self.L)
+
+    def get_curve_length(self) -> float:
+        return self.P * sy.N(sy.sin(self.L) * 2 + sy.cos(self.L) * 2)
+
+
+class Broken_Line_Trajectory(Trajectory):
+    def __init__(self, points, x0=0.0, y0=0.0, z0=0.0):
+        """* points - Точки траектории вида vp.vector(x, y, z);
+        * x0, y0, z0 - смещение траектории по координатной сетке"""
+
+        self.points = points
+        self.N = len(self.points)
+        indices = list(range(self.N)) + [0]
+
+        # self.segments = [
+        #     [points[indices[j]], points[indices[j + 1]]]
+        #     for j in range(len(indices) - 1)
+        # ]
+
+        self.segments = [
+            points[indices[j + 1]] - points[indices[j]] for j in range(len(indices) - 1)
+        ]
+
+        self.vp_vector_length = lambda vec: sy.N(
+            sy.sqrt(vec.x**2 + vec.y**2 + vec.z**2)
+        )
+
+        # self.segment_lengths = [
+        #     self.vp_vector_length(self.segments[i][0] - self.segments[i][1])
+        #     for i in range(self.N)
+        # ]
+
+        self.segment_lengths = [
+            self.vp_vector_length(self.segments[i]) for i in range(self.N)
+        ]
+
+        super().__init__(x0, y0, z0)
+        print(
+            f"Broken_Line_Trajectory(points={points}).curve_length = {self.curve_length}"
+        )
+
+    def get_segment_by_traveled_length(self, length) -> tuple:
+        index = 0
+        while length > self.segment_lengths[index]:
+            length -= self.segment_lengths[index]
+            index += 1
+
+        return index, length
+
+    def get_xyz(self, t) -> tuple:
+        traveled_curve_length = ((t % 360) / 360) * self.curve_length
+        index, traveled_segment_length = self.get_segment_by_traveled_length(
+            traveled_curve_length
+        )
+
+        p = float(traveled_segment_length / self.segment_lengths[index])
+        point = self.points[index] + self.segments[index] * p
+        return (point.x, point.y, point.z)
+
+    def get_curve_length(self) -> float:
+        return sum(self.segment_lengths)
 
 
 def main():
     vp_init()
-
-    global dt
-    dt = 0.005  # The step size. This should be a small number
-    rate = 1000
-    t = 0
-
     world = World()
     world.create_xy_grid(40, 10)
+
+    global dt
+    dt = 0.00005  # The step size. This should be a small number
+    rate = 1000
+    t = 0
 
     bases = [
         Sensor(name=f"static sensor #{i}", pos0=pos)
         for i, pos in enumerate(
             [
-                vp.vector(-20, -20, 0),
-                vp.vector(-20, 20, 0),
-                vp.vector(20, 20, 0),
-                vp.vector(20, -20, 0),
+                # vp.vector(-20, -20, 0),
+                # vp.vector(-20, 20, 0),
+                # vp.vector(20, 20, 0),
+                # vp.vector(20, -20, 0),
+                vp.vector(0, 0, 0),
             ]
         )
     ]
@@ -254,14 +379,22 @@ def main():
         )
         for i, tr in enumerate(
             [
-                Circle_Trajectory(10, -20, -20, 5),
-                Circle_Trajectory(10, -20, 20, 5),
-                Circle_Trajectory(10, 20, 20, 5),
-                Circle_Trajectory(10, 20, -20, 5),
-                Circle_Trajectory(10, -20, -20, 25),
-                Circle_Trajectory(10, -20, 20, 25),
-                Circle_Trajectory(10, 20, 20, 25),
-                Circle_Trajectory(10, 20, -20, 25),
+                # Circle_XY_Trajectory(10, -20, -20, 5),
+                # Circle_XY_Trajectory(10, -20, 20, 5),
+                # Circle_XY_Trajectory(10, 20, 20, 5),
+                # Circle_XY_Trajectory(10, 20, -20, 5),
+                # Circle_XY_Trajectory(10, -20, -20, 25),
+                # Circle_XY_Trajectory(10, -20, 20, 25),
+                # Circle_XY_Trajectory(10, 20, 20, 25),
+                # Circle_XY_Trajectory(10, 20, -20, 25),
+                # Rectangle_XY_Trajectory(20, 0.5)
+                Broken_Line_Trajectory(
+                    [
+                        vp.vector(-10, -10, 0),
+                        vp.vector(-10, 10, 0),
+                        vp.vector(10, 10, 0),
+                    ]
+                )
             ]
         )
     ]
@@ -277,8 +410,16 @@ def main():
             sensor.update_label()
 
         t += dt
-        # vp.scene.caption = f"t = {t / dt}"
-        vp.scene.caption = f"static sensors count: {len(bases)}\ntrajectory sensors count: {len(sensors)}"
+        # vp.scene.caption += (
+        # f"t = {t / dt}\n"
+        # + f"static sensors count: {len(bases)}\n"
+        # + f"trajectory sensors count: {len(sensors)}"
+        # + f"\n"
+        # )
 
 
 main()
+
+# a = vp.vector(1, 1, 1)
+# p = 0.6
+# print(a * p)
